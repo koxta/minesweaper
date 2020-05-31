6aea71dbe41ff8d155f72ff504037d765b4dff7c
Giorgi Maziashvili <koxta@MacBook-Pro.local>
2020-05-31 14:23:49 +0400

    add bomb generation and board fill logic

--- minesweaper.cpp
+++ minesweaper.cpp
@@ -1,11 +1,20 @@
 #include<iostream>
 #include<algorithm>
 #include<vector>
-#include <cstdlib>
-#include <ctime>
+#include<cstdlib>
+#include<ctime>
 using namespace std;
+
+struct Bomb
+{
+    int x;
+    int y;
+};
+
 int boardSize = 8; 
 vector<vector< int > > board;
+vector<Bomb> bombs;
+
 void fillBoardWithZero(){
     board.reserve(boardSize);
     for (int i = 0; i < boardSize; i++)
@@ -29,27 +38,72 @@ void printBoard(){
     }
 }
 
-int random(int m, int mx) { // SUPER IMPORTANT STUFF!!!
-	srand(time(NULL));// SUPER IMPORTANT STUFF!!!
-	int r=mx-m+1; // SUPER IMPORTANT STUFF!!!
-	int num=rand()%r+m;// SUPER IMPORTANT STUFF!!!
-	return num;
+int random(int from, int to) {
+	return rand() % (to+1) + from;
+}
+
+void printBomb(Bomb b){
+    cout<<b.x<<","<<b.y<<endl;
+}
+
+void generateBombs(){
+    for (int i = 0; i < boardSize; i++)
+    {
+        Bomb b;
+        b.x = random(0,boardSize-1);
+        b.y = random(0,boardSize-1);
+        for (int j = 0; j < bombs.size(); j++)
+        {
+           Bomb oldBomb = bombs[j];
+           if(b.x == oldBomb.x && b.y == oldBomb.y){
+               i--;
+               break;
+           }
+        }
+        
+        bombs.push_back(b);
+    }
+}
+
+void fillBoard(){
+    for (int i = 0; i < bombs.size(); i++)
+    {
+        Bomb b = bombs[i];
+
+        int x = b.x;
+        int y = b.y;
+
+        if(x-1>=0){
+            board[x-1][y]++;
+        }
+
+        if(x-1>=0 && y-1>=0){
+            board[x-1][y-1]++;
+        }
+
+        board[x][y-1]++;
+        board[x+1][y+1]++;
+        board[x+1][y]++;
+        board[x+1][y+1]++;
+        board[x][y+1]++;
+        board[x-1][x+1]++;
+    }
+    
+    for (int i = 0; i < bombs.size(); i++)
+    {
+        Bomb b = bombs[i];
+        board[b.x][b.y] = 9;
+    }
 }
 
 int main() {
-    fillBoardWithZero();
-    //printBoard();
-
-
-    // this should be a function
-    unsigned long j;
-
     srand( (unsigned)time(NULL) );
+    fillBoardWithZero();
+    generateBombs();
+    //fillBoard();
 
-    for( j = 0; j < 100500; ++j )
-    {
-        printf( "%d,\t%d\n", rand(), rand() % 6 + 1 );
-    }
+    printBoard();
+
 
     return 0;
 }

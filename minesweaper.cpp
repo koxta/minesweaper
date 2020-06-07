cd6c4cb95c4c98aaee2385b4b65480c32d475438
Giorgi <gmaziashvili@live.com>
2020-06-07 22:07:56 +0400

    update board fill logic

--- minesweaper.cpp
+++ minesweaper.cpp
@@ -12,8 +12,17 @@ struct Bomb
 };
 
 int boardSize = 8; 
-vector<vector< int > > board;
+vector<vector< char > > board;
 vector<Bomb> bombs;
+bool game = true;
+
+int charToInt(char c){
+    return c-48;
+}
+
+char intToChar(int i){
+    return i+48;
+}
 
 void fillBoardWithZero(){
     board.reserve(boardSize);
@@ -22,14 +31,30 @@ void fillBoardWithZero(){
         board[i].reserve(boardSize);
         for (int j = 0; j < boardSize; j++)
         {
-            board[i][j] = 0;
+            board[i][j] = '0';
         }
     }
 }
 
 void printBoard(){
+    char horizontal = 'A';
+    char vertical = '1';
+
+    cout<<"   ";
+    for(int i=0;i<boardSize;i++){
+        cout<<vertical++<<" ";
+    }
+    cout<<endl;
+    cout<<"   ";
+    for(int i=0;i<boardSize;i++){
+        cout<<"-"<<" ";
+    }
+    cout<<endl;
+
+
     for (int i = 0; i < boardSize; i++)
-    {
+    {   
+        cout<<horizontal++<<"| ";
         for (int j = 0; j < boardSize; j++)
         {
             cout<<board[i][j]<<" ";
@@ -50,6 +75,7 @@ void generateBombs(){
     for (int i = 0; i < boardSize; i++)
     {
         Bomb b;
+        bool push = true;
         b.x = random(0,boardSize-1);
         b.y = random(0,boardSize-1);
         for (int j = 0; j < bombs.size(); j++)
@@ -57,11 +83,16 @@ void generateBombs(){
            Bomb oldBomb = bombs[j];
            if(b.x == oldBomb.x && b.y == oldBomb.y){
                i--;
+               push = false;
                break;
            }
         }
-        
-        bombs.push_back(b);
+
+        if (push)
+        {
+            board[b.x][b.y] = '*';
+            bombs.push_back(b);
+        }
     }
 }
 
@@ -74,35 +105,72 @@ void fillBoard(){
         int y = b.y;
 
         if(x-1>=0){
-            board[x-1][y]++;
+            board[x-1][y]++; // left
         }
 
         if(x-1>=0 && y-1>=0){
-            board[x-1][y-1]++;
+            board[x-1][y-1]++; // upper left
         }
 
-        board[x][y-1]++;
-        board[x+1][y+1]++;
-        board[x+1][y]++;
-        board[x+1][y+1]++;
-        board[x][y+1]++;
-        board[x-1][x+1]++;
+        if(y-1 >=0){
+            board[x][y-1]++; // up
+        }
+
+        if(x+1<boardSize && y-1>=0){
+            board[x+1][y-1]++; // upper right
+        }
+
+        if(x+1<boardSize){
+            board[x+1][y]++; // right
+        }
+
+        if(x+1<boardSize && y+1 < boardSize){
+            board[x+1][y+1]++; // lower right
+        }
+
+        if(y+1<boardSize){
+            board[x][y+1]++; // down
+        }
+
+        if(x-1>=0 && y+1 < boardSize){
+            board[x-1][y+1]++; // lower left
+        }
     }
     
     for (int i = 0; i < bombs.size(); i++)
     {
         Bomb b = bombs[i];
-        board[b.x][b.y] = 9;
+        board[b.x][b.y] = '*';
     }
 }
+
+void startGame(){
+    while(game){
+        printBoard();
+        cout<<endl;
+        char c;
+        int n;
+        cout<<"shemoitane koordinatebi: ";
+        cin>>c>>n;
+        int y = c-65;
+        int x = n-1;
+
+        cout<<endl<<x<<" "<<y<<endl;
+        cout<<endl<<board[y][x]<<endl;
+    }
+}
+
+
+
+
 
 int main() {
     srand( (unsigned)time(NULL) );
     fillBoardWithZero();
     generateBombs();
-    //fillBoard();
+    fillBoard();
+    startGame();    
 
-    printBoard();
 
 
     return 0;
